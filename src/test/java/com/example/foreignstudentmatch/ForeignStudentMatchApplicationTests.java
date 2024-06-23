d2f7948b63827744ad18681d7b131cc62396baa0
maark1106 <mark1106@naver.com>
2024-06-23 10:47:38 +0900

    Initial commit

---
+++ src/test/java/com/example/foreignstudentmatch/ForeignStudentMatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.foreignstudentmatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForeignStudentMatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
