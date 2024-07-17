2224f5db096843a4f75e38df7045885fdf2845ac
maark1106 <mark1106@naver.com>
2024-07-17 14:03:16 +0900

    feat : 채팅 메시지 Entity 추가

---
+++ src/main/java/com/example/foreignstudentmatch/domain/ChatMessage.java
@@ -0,0 +1,32 @@
+package com.example.foreignstudentmatch.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class ChatMessage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "chat_room_id")
+    private ChatRoom chatRoom;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id")
+    private Student student;
+
+    private String message;
+    private LocalDateTime timestamp;
+}
