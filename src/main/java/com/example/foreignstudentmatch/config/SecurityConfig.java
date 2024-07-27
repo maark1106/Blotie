22dd493d8b3d1e24f1bb94d057321c84e688b9c1
maark1106 <mark1106@naver.com>
2024-07-27 09:51:40 +0900

    fix : 인증만 모든 사람이 접근할 수 있도록 수정

--- src/main/java/com/example/foreignstudentmatch/config/SecurityConfig.java
+++ src/main/java/com/example/foreignstudentmatch/config/SecurityConfig.java
@@ -1,14 +1,22 @@
 package com.example.foreignstudentmatch.config;
 
+import com.example.foreignstudentmatch.security.CustomUserDetailService;
+import com.example.foreignstudentmatch.security.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final CustomUserDetailService customUserDetailService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -18,9 +26,14 @@ public class SecurityConfig {
                 .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                 .requestMatchers("/auth").permitAll()
                 .requestMatchers("/register").permitAll()
-                .requestMatchers("/api/**").permitAll()
-                .requestMatchers("/ws/chat/**").permitAll()
-                .anyRequest().authenticated(); // 그 외의 모든 요청은 인증이 필요
+//                .requestMatchers("/ws/chat/**").permitAll()
+//                .requestMatchers("/api/**").authenticated() // /api/** 경로는 인증 필요
+                .anyRequest().authenticated()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS); // 세션을 사용하지 않음
+
+        // JWT 필터 추가
+        http.addFilterBefore(new JwtAuthenticationFilter(customUserDetailService), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
