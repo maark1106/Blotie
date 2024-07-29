66b0f55c66043fec824aa1d2a579c2f336167595
maark1106 <mark1106@naver.com>
2024-07-29 20:09:54 +0900

    fix : 배포 환경 환경변수 설정 안되는 오류 수정

--- src/main/java/com/example/foreignstudentmatch/ForeignStudentMatchApplication.java
+++ src/main/java/com/example/foreignstudentmatch/ForeignStudentMatchApplication.java
@@ -1,7 +1,11 @@
 package com.example.foreignstudentmatch;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.DotenvEntry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class ForeignStudentMatchApplication {
@@ -10,4 +14,16 @@ public class ForeignStudentMatchApplication {
 		SpringApplication.run(ForeignStudentMatchApplication.class, args);
 	}
 
+}
+
+@Component
+@Profile("dev")
+class DevEnvLoader {
+	public DevEnvLoader() {
+		Dotenv dotenv = Dotenv.configure().directory("/home/ec2-user/Blotie/.env").load();
+
+		for (DotenvEntry entry : dotenv.entries()) {
+			System.setProperty(entry.getKey(), entry.getValue());
+		}
+	}
 }
