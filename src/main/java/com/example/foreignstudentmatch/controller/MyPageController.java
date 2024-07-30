899a3b214354f5b8a39b279f8625c3a10f54e389
maark1106 <mark1106@naver.com>
2024-07-30 13:05:07 +0900

    fix : mypage buddy 목록 못 가져오는 오류 수정

--- src/main/java/com/example/foreignstudentmatch/controller/MyPageController.java
+++ src/main/java/com/example/foreignstudentmatch/controller/MyPageController.java
@@ -4,10 +4,7 @@ import com.example.foreignstudentmatch.annotation.StudentId;
 import com.example.foreignstudentmatch.dto.ResponseDto;
 import com.example.foreignstudentmatch.service.MyPageService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/mypage")
@@ -16,8 +13,8 @@ public class MyPageController {
 
     private final MyPageService MyPageService;
 
-    @GetMapping
-    public ResponseDto<?> getStudentMyPage(@StudentId Long studentId) {
+    @GetMapping("/{studentId}")
+    public ResponseDto<?> getStudentMyPage(@StudentId Long id, @PathVariable("studentId") Long studentId) {
         return new ResponseDto<>(MyPageService.getStudentMyPage(studentId));
     }
 }
